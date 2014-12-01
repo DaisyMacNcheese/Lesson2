7028b8e1dfbd840c280400c69bcd05dda2b08cd0
DaisyMacNcheese <DaisyMacNcheese@users.noreply.github.com>
2014-12-01 12:02:07 -0500

    typo
    typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='Simple Flask web application with user input',
     author='DaisyMacNcheese',
-    url='https://github.com/jfalken/nessus_enterprise_rest_client',
+    url='https://github.com/DaisyMacNcheese/Lesson2',
     install_requires=['Flask','Jinja2','MarkupSafe','Werkzeug','itsdangerous','wsgiref'],
     py_modules=['homework2']
 )
