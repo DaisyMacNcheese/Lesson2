49dbbefe3095db580f75cfe2e22344865a621486
DaisyMacNcheese <DaisyMacNcheese@users.noreply.github.com>
2014-12-01 13:05:38 -0500

    typo
    typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='Homework2',
+    name='homework2',
     version='0.1',
     description='Simple Flask web application with user input',
     author='DaisyMacNcheese',
